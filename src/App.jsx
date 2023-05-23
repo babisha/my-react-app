c8453e3dfeed7a3b37ffbe7971790032850abd8b
babisha <mdr.bebo@gmail.com>
2023-05-22 21:45:47 -0400

    basic css and flex

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="box">
+        <div className="circle"></div>
+        <header>
+          <a href="https://1.bp.blogspot.com/-8PrEAy5iJoE/X8uQXdyo-RI/AAAAAAAAKFQ/ZNPEqgvMLoM9yIboctX4T7z3J83xnLlqgCLcBGAsYHQ/s1600/bigstock-Rock-Cycle-Vector-Illustration-325357108.jpg"></a></header>
       </div>
-      <h1>Good Evening</h1>
-      <h2>Happy Friday, Have a good one!</h2>
-      <div className="card">
-        
-        <p>
-         It was a pleasure learning about react component.
-        </p>
-      </div>
+      <article>
+        <header>
+          <h1>The Infinite Cycle of Rock Formation</h1>
+        </header>
+      </article>
       <p className="read-the-docs">
-        
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </>
   )
